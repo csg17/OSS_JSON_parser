c20ffde71acc7f9c70eaa94214c08d2efa5537de
csg17 <47957052+csg17@users.noreply.github.com>
2019-05-24 17:44:00 +0900

    print string token without printing the string array

--- main.c
+++ main.c
@@ -29,26 +29,26 @@ int main(int argc, char **argv) {
 
     FILE *fp = NULL;
     TOKEN_T *tokenList = malloc(sizeof(TOKEN_T) * TOKEN_COUNT);
-    
+
     if(argc < 2){
         printf("usage: ./out <filename>\n");
         return -1;
     }
-    
+
     fp = fopen(argv[1], "r");
     if(fp == NULL){
         printf("fail to open file %s\n", argv[1]);
         return -1;
     }
-    
+
     sizeOfFile = getFileSize(argv[1]);
     char allContent[sizeOfFile];
-    
+
     while(fscanf(fp, "%s", buffer) != EOF) {
         strcat(allContent, buffer);
         strcat(allContent, " ");
     }
-    
+
     JsonParser(allContent, sizeOfFile, tokenList);
 
     return 0;
@@ -61,10 +61,10 @@ int getFileSize(char *filename){
     if(fp == NULL) {
         printf("fail to open file\n");
     } else {
-        fseek(fp, 0, SEEK_END); 
-        len = (int)ftell(fp); 
+        fseek(fp, 0, SEEK_END);
+        len = (int)ftell(fp);
     }
-    
+
     return len;
 }
 
@@ -72,13 +72,13 @@ int getFileSize(char *filename){
 void JsonParser(char *allContent, int contentSize, TOKEN_T *list){
     int cur = 1;
     int numOfToken = 0;
-    
+
     if(allContent[cur] != '{'){
         return ;
     }
-    
+
     cur++;
-    
+
     while(cur < contentSize){
         switch (allContent[cur]) { //doc[pos]
             case '"':
@@ -88,15 +88,15 @@ void JsonParser(char *allContent, int contentSize, TOKEN_T *list){
                 char temp[contentSize];
 
                 if(end == NULL) break;
-                
+
                 int wordLen = end - begin;
-                
+
                 strncpy(temp, begin, wordLen);
                 temp[wordLen] = '\0';
                 numOfToken++;
-                
+
                 //print info of each token
-                printf("[%2d] %s (size=0, %lu~%lu)\n", numOfToken, temp, strlen(allContent) - strlen(begin), strlen(allContent) - strlen(end));
+                printf("[%2d] %s (size=1, %lu~%lu)\n", numOfToken, temp, strlen(allContent) - strlen(begin), strlen(allContent) - strlen(end));
 
                 cur = cur + wordLen + 1;
                 //tokenList에 저장하는거
@@ -106,9 +106,10 @@ void JsonParser(char *allContent, int contentSize, TOKEN_T *list){
             case '[':
             {
                 cur++;
-                char temp[contentSize];
+                char temp[contentSize]; // doc[pos]
 
                 while( allContent[cur] != ']' ){
+                  cur++;
                     if( allContent[cur] == '"')
                     {
                         char *begin = allContent + cur + 1;
@@ -116,47 +117,21 @@ void JsonParser(char *allContent, int contentSize, TOKEN_T *list){
                         if( end==NULL ) break;
 
                         int wordLen = end - begin;
-
                         strncpy(temp, begin, wordLen);
                         temp[wordLen] = '\0';
                         cur = cur + wordLen + 1;
-                        printf("[%2d] %s (size=0, %lu~%lu)\n", numOfToken, temp, strlen(allContent) - strlen(begin), strlen(allContent) - strlen(end));
+                        numOfToken++;
+                        printf("[%2d] %s (size=1, %lu~%lu)\n", numOfToken, temp, strlen(allContent) - strlen(begin), strlen(allContent) - strlen(end));
                     }
-                    numOfToken++;
                     //print info of each token
                 }
+
             break;
             }
-	case '0': case '1': case '2': case '3': case '4': case '5':
-	case '6': case '7': case '8': case '9': case '-':
-	    {
-		    char *begin = allContent + cur;
-		    char *end;
-		    char *buffer;
-
-		    end = strchr(allContent + cur, '}');
-		    if (end == NULL)
-			    break;
-	    }
-
-	    int stringLength = end -begin;
-
-	    buffer = malloc(stringLength +1);
-	    memset(buffer,0,stringLength +1);
-	    memcpy(buffer, begin,stringLength);
-	    json->tokens[tokenIndex].type = TOKEN_NUMBER;
-	    json->toekns[tokenIndex].number = atof(buffer);
-
-	    free(buffer);
-	    tokenIndex++;
-
-	    cur = cur + stringLength +1;
-	}
             default:
                 break;
         }
         cur++;
     }
-    
-}
 
+}
